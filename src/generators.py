f10b89b89bb6ec9cb1a60e5593ff835b7a3fc161
Giorgos Papoudakis <giwrpapoud@gmail.com>
2017-09-14 15:13:56 +0200

    add esdg

--- src/generators.py
+++ src/generators.py
@@ -3,7 +3,8 @@ import numpy as np
 import random
 
 
-def generator(num_nodes, num_edges, epsilon1, epsilon2):
+# sparse digraph generator (SDG)
+def sdg(num_nodes, num_edges, epsilon1, epsilon2):
     gen_graph = nx.DiGraph()
     nodes = range(num_nodes)
     gen_graph.add_nodes_from(nodes)
@@ -44,6 +45,7 @@ def generator(num_nodes, num_edges, epsilon1, epsilon2):
     return gen_graph
 
 
+# Bollobas graph generator
 def bollobas(num_edges, alpha, beta, delta1=1.0, delta2=1.0):
     i = 0
     gen_graph = nx.DiGraph()
@@ -89,3 +91,99 @@ def bollobas(num_edges, alpha, beta, delta1=1.0, delta2=1.0):
     return gen_graph
 
 
+def esdg(gen_graph, num_of_new_edges, new_nodes, epsilon1, epsilon2, alpha, beta):
+    j = 0
+    gen_graph.add_nodes_from(new_nodes)
+
+    # total nodes of the graph
+    nodes = gen_graph.nodes()
+    out_added_new = False
+    in_added_new = False
+    while j < num_of_new_edges:
+        p = random.random()
+        if p < alpha:
+            # choose out from the new nodes
+
+            if random.random() < epsilon1 or not out_added_new:
+                out_node = random.choice(new_nodes)
+            else:
+                out_nodes = new_nodes
+                out_degree = np.array(gen_graph.out_degree(new_nodes).values())
+                out_node = np.random.choice(out_nodes, p=out_degree.astype('float32') / np.sum(out_degree))
+            # choose in for all the nodes
+            if random.random() < epsilon2:
+
+                if 0 not in gen_graph.in_degree().values():
+                    in_node = random.choice(nodes)
+                else:
+
+                    if len(nx.isolates(gen_graph)) > 0:
+                        in_node = random.choice(nx.isolates(gen_graph))
+                    else:
+                        in_node_degree = 1
+                        while in_node_degree > 0:
+                            in_node = random.choice(gen_graph.nodes())
+                            in_node_degree = gen_graph.in_degree(in_node)
+            else:
+                in_nodes = nodes
+                in_degree = np.array(gen_graph.in_degree(nodes).values())
+                in_node = np.random.choice(in_nodes, p=in_degree.astype('float32') / np.sum(in_degree))
+        elif alpha < p < alpha + beta:
+            # choose out from all nodes
+            if random.random() < epsilon1:
+                out_node = random.choice(nodes)
+            else:
+                out_nodes = gen_graph.out_degree().keys()
+                out_degree = np.array(gen_graph.out_degree().values())
+                out_node = np.random.choice(out_nodes, p=out_degree.astype('float32') / np.sum(out_degree))
+            # choose in from new nodes
+            if random.random() < epsilon2 or not in_added_new:
+                if 0 not in gen_graph.in_degree(new_nodes).values():
+                    in_node = random.choice(new_nodes)
+                else:
+                    if len(nx.isolates(gen_graph)) > 0:
+                        in_node = random.choice(nx.isolates(gen_graph))
+                    else:
+                        in_node_degree = 1
+                        while in_node_degree > 0:
+                            in_node = random.choice(new_nodes)
+                            in_node_degree = gen_graph.in_degree(in_node)
+            else:
+                in_nodes = new_nodes
+                in_degree = np.array(gen_graph.in_degree(new_nodes).values())
+                in_node = np.random.choice(in_nodes, p=in_degree.astype('float32') / np.sum(in_degree))
+
+        else:
+            # choose both in and out from all nodes
+            if random.random() < epsilon1:
+                out_node = random.choice(nodes)
+            else:
+                out_nodes = gen_graph.out_degree().keys()
+                out_degree = np.array(gen_graph.out_degree().values())
+                out_node = np.random.choice(out_nodes, p=out_degree.astype('float32') / np.sum(out_degree))
+            if random.random() < epsilon2:
+                if 0 not in gen_graph.in_degree().values():
+                    in_node = random.choice(nodes)
+                else:
+
+                    if len(nx.isolates(gen_graph)) > 0:
+                        in_node = random.choice(nx.isolates(gen_graph))
+                    else:
+                        in_node_degree = 1
+                        while in_node_degree > 0:
+                            in_node = random.choice(gen_graph.nodes())
+                            in_node_degree = gen_graph.in_degree(in_node)
+            else:
+                in_nodes = gen_graph.in_degree().keys()
+                in_degree = np.array(gen_graph.in_degree().values())
+                in_node = np.random.choice(in_nodes, p=in_degree.astype('float32') / np.sum(in_degree))
+
+        if out_node != in_node and (out_node, in_node) not in gen_graph.edges():
+            gen_graph.add_edge(out_node, in_node)
+            j += 1
+        # just to make sure that the model wont use preferential attachment if there are no available nodes
+        if out_node in new_nodes:
+            out_added_new = True
+        if in_node in new_nodes:
+            in_added_new = True
+    return gen_graph
